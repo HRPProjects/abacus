72972c37b18a198bc442ae299310a8d4c7c2c823
hromero73 <hugooromero73@gmail.com>
2020-10-17 21:15:43 -0700

    Remove usage of a reserved word

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -9,15 +9,23 @@ class Compute:
         pass
 
     def substact(self):
-        pass
+        difference = 0
+    
+        for item in self.operands
+            difference -= item
+
+        print(difference) 
 
     def divide(self):
         pass
 
     def multiply(self):
-        sum = 1
+        if self.operands is None
+           return
+
+        product = 1
 
         for item in self.operands
-            sum *= item
+            product *= item
 
-        print(sum)
+        print(product)
